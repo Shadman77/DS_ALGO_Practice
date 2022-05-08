f5acdd0917ef4f5811592a7ccc4c777cfd155772
Shadman Saif Anonno <anonno77@gmail.com>
2022-05-08 09:24:33 +0600

    nth add

--- Linked List/Singly.py
+++ Linked List/Singly.py
@@ -22,7 +22,6 @@ def traverse(header):
     print(header.get_value())
     if header.get_next_node_addr() == None:
         print()
-        return
     else:
         traverse(header.get_next_node_addr())
 
@@ -61,6 +60,19 @@ def delete_nth(header, n, i=1):
     else:
         delete_nth(next_node, n, i + 1)
 
+def add_nth(node, value, n, i = 1, prev_node = None):
+    if n == i:
+        if prev_node == None:
+            new_node = SinglyNode(value=node.get_value(), next_node_addr=node.get_next_node_addr())
+            node.set_value(value)
+            node.set_next_node_addr(new_node)
+        else:
+            new_node = SinglyNode(value=value, next_node_addr=node)
+            prev_node.set_next_node_addr(new_node)
+    else:
+        add_nth(node.get_next_node_addr(), value, i+1, node)
+
+
 
 if __name__ == "__main__":
     header = SinglyNode(1)
@@ -69,18 +81,30 @@ if __name__ == "__main__":
     add(header, 4)
     add(header, 5)
     traverse(header)
+
+
     delete_last(header)
     delete_last(header)
     delete_last(header)
     delete_last(header)
     delete_last(header)
     traverse(header)
+    
     add(header, 5)
     for i in range(1, 5):
         add(header, i)
     traverse(header)
+    
     delete_nth(header, 2)
     traverse(header)
+    
+    delete_nth(header, 1)
+    delete_nth(header, 1)
+    delete_nth(header, 1)
     delete_nth(header, 1)
     traverse(header)
+    
+    add_nth(header, 1, 1)
+    traverse(header)
+    
     # print(header.get_value())
