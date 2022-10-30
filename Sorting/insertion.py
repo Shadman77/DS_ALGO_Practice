e2a91689d4ea6dcf2f2d3bb1f3b2d99eac68bb75
Shadman Saif <shadman.saif@arenaphonebd.net>
2022-10-30 12:48:34 +0600

    insertion sort on going

---
+++ Sorting/insertion.py
@@ -0,0 +1,16 @@
+#in each iter put the less valued element in place
+
+def insertion_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            
+        
+
+    return arr
+
+
+if __name__ == "__main__":
+    arr = [10, 8, 5, 6, 4, 1, 2, 3, 7, 9]
+
+    print(arr)
+    print(insertion_sort(arr))
