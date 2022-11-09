2c26fb75b7eca8b11fcbe1a9d69a2b8c80ee0eb5
Shadman Saif <shadman.saif@arenaphonebd.net>
2022-11-09 18:19:59 +0600

    trie ongoing

--- Searching/trie.py
+++ Searching/trie.py
@@ -5,11 +5,22 @@ class node:
         self.__next_chars_mapping = next_chars_mapping
     
     def get_next(self, char):
-        return self.__next_chars_mapping[char]
+        if char in self.__next_chars_mapping.keys():
+            return self.__next_chars_mapping[char]
+        else:
+            return None
     
     def insert(self, char, node):
         self.__next_chars_mapping[char] = node
 
+    def check_if_end_of_word(self):
+        return self.__end_of_a_word
+
 if __name__ == "__main__":
      start = {}
+
+     new_word = "start"
+     for i in range(len(new_word)-1):
+        if new_word[i] in start.keys():
+            node = start.keys
      
