d8fd02ccfffb9829c1cadefb4cdbfdafe08f3c97
Shadman Saif <anonno77@gmail.com>
2024-06-20 10:22:16 +0600

    Bubble acsending and desending

--- Sorting/bubble.ts
+++ Sorting/bubble.ts
@@ -1,10 +1,21 @@
-const bubbleSort = (arr: number[]): number[] => {
+enum SortDirection {
+  Asc = "asc",
+  Desc = "dsc",
+}
+
+const bubbleSort = (arr: number[], direction: SortDirection): number[] => {
   const ans: number[] = arr.map((element) => element);
   for (let index = 0; index < ans.length; index++) {
     for (let indexTwo = 0; indexTwo < ans.length - index; indexTwo++) {
       const firstElement: number = ans[indexTwo];
       const secondElement: number = ans[indexTwo + 1];
-      if (firstElement > secondElement) {
+
+      const condition: boolean =
+        direction === SortDirection.Asc
+          ? firstElement > secondElement
+          : secondElement > firstElement;
+
+      if (condition) {
         ans[indexTwo] = secondElement;
         ans[indexTwo + 1] = firstElement;
       }
@@ -14,6 +25,7 @@ const bubbleSort = (arr: number[]): number[] => {
 };
 
 const arr: number[] = [10, 8, 5, 6, 4, 1, 2, 3, 7, 9];
-const ans: number[] = bubbleSort(arr);
+const ansAsc: number[] = bubbleSort(arr, SortDirection.Asc);
+const ansDsc: number[] = bubbleSort(arr, SortDirection.Desc);
 
-console.log(arr, ans);
+console.log(arr, ansAsc, ansDsc);
